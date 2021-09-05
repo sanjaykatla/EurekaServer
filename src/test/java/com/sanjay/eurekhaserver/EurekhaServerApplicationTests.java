c3ebfb6ded2d8aa052bf981a05fb490b646449be
Sanjay Katla <sanjay_katla@mentor.com>
2021-09-05 23:48:46 +0530

    This is simple Eureka Server Spring Boot Application to discover MicroService1, MicroService2, and API Gateway Service

---
+++ src/test/java/com/sanjay/eurekhaserver/EurekhaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjay.eurekhaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekhaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
